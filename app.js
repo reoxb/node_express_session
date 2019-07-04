cd93a726f6f4a8028901025c5e942111a5cb25d3
SUAREZ SOSA <J.S.SOSA@CIMMYT.ORG>
2019-07-04 15:44:29 -0500

    Agrega la session a base de datos

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 const express = require('express');
-// necesario para almacenar la sesion 
 const session = require('express-session')
 const app = express();
+const mongoose = require('mongoose');
+const mongoStore = require('connect-mongo')(session);
+
 
 const port = 9000;
 const hostname = 'localhost';
+
+// Connect to DB
+mongoose.connect('mongodb://localhost/userSession',
+  function(err, res) {
+    if(err) throw err;
+    console.log('Conectado con éxito a la BD');
+});
+
 
 // definimos como manejara express la session
 app.use(session({
     secret: 'javascript rules',
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {maxAge: 24 * 60 * 60 * 1000},
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection,
+      clear_interval: (24 * 60 * 60 * 100)
+    })
 }));
 
 app.get('/', function (req, res) {
+    // establece una variable de la sesion para ser consultada y almacenada
     req.session.cuenta = req.session.cuenta ? req.session.cuenta + 1 : 1;
+    console.log(req.session);
     res.send(`Numero de visitas en la pagina ${req.session.cuenta}`)    
 })
 
